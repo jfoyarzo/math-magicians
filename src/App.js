329b5841224e7a7a8ebc3319df77bc8d13e78019
jfoyarzo <foyarzo621@gmail.com>
2022-10-25 23:24:18 -0300

    Update App.js to display the correct site title

--- src/App.js
+++ src/App.js
@@ -1,6 +1,10 @@
+import { useEffect } from 'react';
 import Calculator from './components/Calculator';
 
 function App() {
+  useEffect(() => {
+    document.title = 'Math Magicians';
+  });
   return (
     <div className="App">
       <Calculator />
