210ecbab3da2809a41e9b49d3b316635ddf258e5
jfoyarzo <foyarzo621@gmail.com>
2022-10-26 21:59:02 -0300

    Add state and handleButtonClick method to main component App.js

--- src/App.js
+++ src/App.js
@@ -6,12 +6,32 @@ import Calculator from './components/Calculator';
 class App extends React.Component {
   constructor(props) {
     super(props);
+    this.handleButtonClick = this.handleButtonClick.bind(this);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+  }
+
+  handleButtonClick(newObj) {
+    this.setState({
+      total: newObj.total,
+      next: newObj.next,
+      operation: newObj.operation,
+    });
   }
 
   render() {
+    const { total, next, operation } = this.state;
     return (
       <div className="App">
-        <Calculator />
+        <Calculator
+          total={total}
+          next={next}
+          operation={operation}
+          handleButtonClick={this.handleButtonClick}
+        />
       </div>
     );
   }
