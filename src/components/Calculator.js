d1512ce22e9d745a49ccac09947fe81c5574c3a5
jfoyarzo <foyarzo621@gmail.com>
2022-10-26 22:47:33 -0300

    Update Calculator component with button tags instead of span

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -26,24 +26,24 @@ export default class Calculator extends React.Component {
         <div className="result">0</div>
         <div className="buttons">
           <button type="button" className="button resetBtn" onClick={this.handleButtonClick}>AC</button>
-          <span className="button negative operator">+/-</span>
-          <span className="button operator percent">%</span>
-          <span className="button operator">/</span>
-          <span className="button number">7</span>
-          <span className="button number">8</span>
-          <span className="button number">9</span>
-          <span className="button operator">*</span>
-          <span className="button number">4</span>
-          <span className="button number">5</span>
-          <span className="button number">6</span>
-          <span className="button operator">-</span>
-          <span className="button number">1</span>
-          <span className="button number">2</span>
-          <span className="button number">3</span>
-          <span className="button operator">+</span>
-          <span className="button number zero-btn">0</span>
-          <span className="button number dot">.</span>
-          <span className="button operator">=</span>
+          <button type="button" className="button negative operator">+/-</button>
+          <button type="button" className="button operator percent">%</button>
+          <button type="button" className="button operator">/</button>
+          <button type="button" className="button number">7</button>
+          <button type="button" className="button number">8</button>
+          <button type="button" className="button number">9</button>
+          <button type="button" className="button operator">*</button>
+          <button type="button" className="button number">4</button>
+          <button type="button" className="button number">5</button>
+          <button type="button" className="button number">6</button>
+          <button type="button" className="button operator">-</button>
+          <button type="button" className="button number">1</button>
+          <button type="button" className="button number">2</button>
+          <button type="button" className="button number">3</button>
+          <button type="button" className="button operator">+</button>
+          <button type="button" className="button number zero-btn">0</button>
+          <button type="button" className="button number dot">.</button>
+          <button type="button" className="button operator">=</button>
         </div>
       </div>
     );
