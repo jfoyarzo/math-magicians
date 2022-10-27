2028015c2fdffe692a0f993f6e2a5e8838fc8ed2
jfoyarzo <foyarzo621@gmail.com>
2022-10-26 22:45:04 -0300

    Add static method handleButtonClick to Calculator.js

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -2,10 +2,22 @@
 /* eslint-disable react/prefer-stateless-function */
 import React from 'react';
 import './calculator.css';
+import calculate from '../logic/calculate';
 
 export default class Calculator extends React.Component {
   constructor(props) {
     super(props);
+  }
+
+  static handleButtonClick(e, props) {
+    const buttonName = e.target.value;
+    const obj = {
+      total: props.total,
+      next: props.next,
+      operation: props.operation,
+    };
+    const newObj = calculate(obj, buttonName);
+    props.handleButtonClick(newObj);
   }
 
   render() {
@@ -13,7 +25,7 @@ export default class Calculator extends React.Component {
       <div className="container">
         <div className="result">0</div>
         <div className="buttons">
-          <span className="button resetBtn">AC</span>
+          <button type="button" className="button resetBtn" onClick={this.handleButtonClick}>AC</button>
           <span className="button negative operator">+/-</span>
           <span className="button operator percent">%</span>
           <span className="button operator">/</span>
