0fe7cb1a355811fcd3c105176d860523821d0cfc
jfoyarzo <foyarzo621@gmail.com>
2022-11-03 19:02:05 -0300

    Add content to calculator component

--- src/components/Calculator.js
+++ src/components/Calculator.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import './calculator.css';
+import '../styles/calculator.css';
 import calculate from '../logic/calculate';
 
 const Calculator = () => {
@@ -10,28 +10,31 @@ const Calculator = () => {
     setState(calculate(state, buttonName));
   };
   return (
-    <div className="container">
-      <div className="result">{ state.next || state.operation || state.total}</div>
-      <div className="buttons">
-        <button type="button" className="button resetBtn" onClick={handleButtonClick}>AC</button>
-        <button type="button" className="button negative operator" onClick={handleButtonClick}>+/-</button>
-        <button type="button" className="button operator percent" onClick={handleButtonClick}>%</button>
-        <button type="button" className="button operator" onClick={handleButtonClick}>÷</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>7</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>8</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>9</button>
-        <button type="button" className="button operator" onClick={handleButtonClick}>x</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>4</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>5</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>6</button>
-        <button type="button" className="button operator" onClick={handleButtonClick}>-</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>1</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>2</button>
-        <button type="button" className="button number" onClick={handleButtonClick}>3</button>
-        <button type="button" className="button operator" onClick={handleButtonClick}>+</button>
-        <button type="button" className="button number zero-btn" onClick={handleButtonClick}>0</button>
-        <button type="button" className="button number dot" onClick={handleButtonClick}>.</button>
-        <button type="button" className="button operator" onClick={handleButtonClick}>=</button>
+    <div className="flex-wrapper">
+      <h2>Let&apos;s do some Math!</h2>
+      <div className="calc-container">
+        <div className="result">{ state.next || state.operation || state.total}</div>
+        <div className="buttons">
+          <button type="button" className="button resetBtn" onClick={handleButtonClick}>AC</button>
+          <button type="button" className="button negative operator" onClick={handleButtonClick}>+/-</button>
+          <button type="button" className="button operator percent" onClick={handleButtonClick}>%</button>
+          <button type="button" className="button operator" onClick={handleButtonClick}>÷</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>7</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>8</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>9</button>
+          <button type="button" className="button operator" onClick={handleButtonClick}>x</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>4</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>5</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>6</button>
+          <button type="button" className="button operator" onClick={handleButtonClick}>-</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>1</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>2</button>
+          <button type="button" className="button number" onClick={handleButtonClick}>3</button>
+          <button type="button" className="button operator" onClick={handleButtonClick}>+</button>
+          <button type="button" className="button number zero-btn" onClick={handleButtonClick}>0</button>
+          <button type="button" className="button number dot" onClick={handleButtonClick}>.</button>
+          <button type="button" className="button operator" onClick={handleButtonClick}>=</button>
+        </div>
       </div>
     </div>
   );
--- src/index.js
+++ src/index.js
@@ -1,17 +1,11 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import Calculator from './components/Calculator';
 
-const ChangeTitle = () => {
-  useEffect(() => {
-    document.title = 'Math Magicians';
-  });
-};
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <ChangeTitle />
-    <App />
+    <Calculator />
   </React.StrictMode>,
 );
