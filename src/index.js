a4217b1a156c3f4f780ea93ec0ffd6d4d6fa581a
jfoyarzo <foyarzo621@gmail.com>
2022-11-03 19:46:52 -0300

    Add Navbar component

--- src/index.js
+++ src/index.js
@@ -1,11 +1,18 @@
 import React, { useEffect } from 'react';
 import ReactDOM from 'react-dom/client';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 import './index.css';
 import Calculator from './components/Calculator';
+import Navbar from './components/Navbar';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <Calculator />
+    <Router>
+      <Navbar />
+      <Routes>
+        <Route exact path="/" element={<Calculator />} />
+      </Routes>
+    </Router>
   </React.StrictMode>,
 );
